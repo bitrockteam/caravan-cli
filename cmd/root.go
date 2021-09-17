1341f09c23bb1d80f170fe5599c51f3de9887c73
Andrea Simonini <andrea.simonini@gmail.com>
2021-09-17 16:19:30 +0200

    disable usage display on every error

--- cmd/root.go
+++ cmd/root.go
@@ -25,6 +25,7 @@ var rootCmd = &cobra.Command{
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+	SilenceUsage: true,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
