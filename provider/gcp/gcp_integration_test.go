11911c3f7a7fb23a3ba2a31f32c143a1450cb1ea
Andrea Simonini <andrea.simonini@gmail.com>
2021-10-07 15:11:45 +0200

    provider/gcp fix integration tests

--- provider/gcp/gcp_integration_test.go
+++ provider/gcp/gcp_integration_test.go
@@ -4,8 +4,9 @@
 package gcp_test
 
 import (
-	"caravan/internal/caravan"
-	"caravan/internal/gcp"
+	"caravan-cli/cli"
+	"caravan-cli/provider/gcp"
+	"context"
 	"strings"
 	"testing"
 
@@ -15,7 +16,7 @@ import (
 func TestProject(t *testing.T) {
 	uid := strings.Split(uuid.New().String(), "-")[0]
 	name := "name-" + uid
-	c, err := caravan.NewConfigFromScratch(name, "gcp", "europe-west6")
+	c, err := cli.NewConfigFromScratch(name, "gcp", "europe-west6")
 	if err != nil {
 		t.Fatalf("unable to create config: %s\n", err)
 	}
@@ -23,23 +24,24 @@ func TestProject(t *testing.T) {
 	c.SetGCPOrgID("55685363496")
 	c.SetGCPBillingID("016290-A416F4-EC4527")
 
-	g, err := gcp.New(c)
+	ctx := context.Background()
+	g, err := gcp.New(ctx, c)
 	if err != nil {
 		t.Fatalf("unable to create GCP: %s\n", err)
 	}
-	if err := g.CreateProject(name, c.GCPOrgID); err != nil {
+	if err := g.CreateProject(ctx, name, c.GCPOrgID); err != nil {
 		t.Fatalf("unable to create project: %s\n", err)
 	}
 	// idempotence
-	if err = g.CreateProject(name, c.GCPOrgID); err != nil {
+	if err = g.CreateProject(ctx, name, c.GCPOrgID); err != nil {
 		t.Fatalf("unable to create project: %s\n", err)
 	}
-	p, err := g.GetProject(name, c.GCPOrgID)
+	p, err := g.GetProject(ctx, name, c.GCPOrgID)
 	if err != nil {
 		t.Fatalf("unable to Get project: %s\n", err)
 	}
 
-	if err := g.SetBillingAccount(name, c.GCPBillingID); err != nil {
+	if err := g.SetBillingAccount(ctx, name, c.GCPBillingID); err != nil {
 		t.Errorf("unable to set billing ID: %s", err)
 	}
 
@@ -47,11 +49,11 @@ func TestProject(t *testing.T) {
 		t.Errorf("want %s,%s got %s,%s\n", name, c.GCPOrgID, p.Name, p.Parent)
 	}
 
-	if err := g.DeleteProject(name, c.GCPOrgID); err != nil {
+	if err := g.DeleteProject(ctx, name, c.GCPOrgID); err != nil {
 		t.Fatalf("unable to delete project %s: %s\n", name, err)
 	}
 	//idempotence
-	if err := g.DeleteProject(name, c.GCPOrgID); err != nil {
+	if err := g.DeleteProject(ctx, name, c.GCPOrgID); err != nil {
 		t.Fatalf("unable to delete project %s: %s\n", name, err)
 	}
 }
@@ -59,36 +61,37 @@ func TestProject(t *testing.T) {
 func TestStateStore(t *testing.T) {
 	uid := strings.Split(uuid.New().String(), "-")[0]
 	name := "name-" + uid
-	c, err := caravan.NewConfigFromScratch("andrea-test-008", "gcp", "europe-west6")
+	c, err := cli.NewConfigFromScratch("andrea-test-015", "gcp", "europe-west6")
 	if err != nil {
 		t.Fatalf("unable to create config: %s\n", err)
 	}
 
-	g, err := gcp.New(c)
+	ctx := context.Background()
+	g, err := gcp.New(ctx, c)
 	if err != nil {
 		t.Fatalf("unable to create GCP: %s\n", err)
 	}
 	// create bucket
-	if err := g.CreateStateStore(name); err != nil {
+	if err := g.CreateStateStore(ctx, name); err != nil {
 		t.Errorf("unable to create the bucket: %s", err)
 	}
-	if err := g.CreateStateStore(name); err != nil {
+	if err := g.CreateStateStore(ctx, name); err != nil {
 		t.Errorf("unable to create the bucket: %s", err)
 	}
 
-	if err := g.WriteStateStore(name, name, "some data"); err != nil {
+	if err := g.WriteStateStore(ctx, name, name, "some data"); err != nil {
 		t.Errorf("unable get write: %s", err)
 	}
 
-	if err := g.EmptyStateStore(name); err != nil {
+	if err := g.EmptyStateStore(ctx, name); err != nil {
 		t.Errorf("unable to empty state store: %s\n", err)
 	}
 
 	// delete bucket
-	if err := g.DeleteStateStore(name); err != nil {
+	if err := g.DeleteStateStore(ctx, name); err != nil {
 		t.Errorf("unable to create the bucket: %s", err)
 	}
-	if err := g.DeleteStateStore(name); err != nil {
+	if err := g.DeleteStateStore(ctx, name); err != nil {
 		t.Errorf("unable to create the bucket: %s", err)
 	}
 }
@@ -96,31 +99,32 @@ func TestStateStore(t *testing.T) {
 func TestServiceAccount(t *testing.T) {
 	uid := strings.Split(uuid.New().String(), "-")[0]
 	name := "name-" + uid
-	c, err := caravan.NewConfigFromScratch("andrea-test-008", "gcp", "europe-west6")
+	c, err := cli.NewConfigFromScratch("andrea-test-015", "gcp", "europe-west6")
 	if err != nil {
 		t.Fatalf("unable to create config: %s\n", err)
 	}
 
-	g, err := gcp.New(c)
+	ctx := context.Background()
+	g, err := gcp.New(ctx, c)
 	if err != nil {
 		t.Fatalf("unable to create GCP: %s\n", err)
 	}
 
-	if err := g.CreateServiceAccount(name); err != nil {
+	if err := g.CreateServiceAccount(ctx, name); err != nil {
 		t.Fatalf("unable to create service account: %s\n", err)
 	}
-	if err := g.CreateServiceAccount(name); err != nil {
+	if err := g.CreateServiceAccount(ctx, name); err != nil {
 		t.Fatalf("unable to create service account: %s\n", err)
 	}
-	_, err = g.CreateServiceAccountKeys(name, name)
+	_, err = g.CreateServiceAccountKeys(ctx, name, name)
 	if err != nil {
 		t.Errorf("unable to create service account key: %s\n", err)
 	}
 
-	if err := g.DeleteServiceAccount(name); err != nil {
+	if err := g.DeleteServiceAccount(ctx, name); err != nil {
 		t.Fatalf("unable to delete service account: %s\n", err)
 	}
-	if err := g.DeleteServiceAccount(name); err != nil {
+	if err := g.DeleteServiceAccount(ctx, name); err != nil {
 		t.Fatalf("unable to delete service account: %s\n", err)
 	}
 }
@@ -128,39 +132,41 @@ func TestServiceAccount(t *testing.T) {
 func TestAddPolicy(t *testing.T) {
 	uid := strings.Split(uuid.New().String(), "-")[0]
 	name := "name-" + uid
-	name = "andrea-test-008-terraform"
-	c, err := caravan.NewConfigFromScratch("andrea-test-008", "gcp", "europe-west6")
+	name = "andrea-test-015-terraform"
+	c, err := cli.NewConfigFromScratch("andrea-test-015", "gcp", "europe-west6")
 	if err != nil {
 		t.Fatalf("unable to create config: %s\n", err)
 	}
 
-	g, err := gcp.New(c)
+	ctx := context.Background()
+	g, err := gcp.New(ctx, c)
 	if err != nil {
 		t.Fatalf("unable to create GCP: %s\n", err)
 	}
-	if err := g.CreateServiceAccount(name); err != nil {
+	if err := g.CreateServiceAccount(ctx, name); err != nil {
 		t.Fatalf("unable to create service account: %s\n", err)
 	}
 	member := "serviceAccount:" + name + "@" + g.Caravan.Name + ".iam.gserviceaccount.com"
-	if err := g.AddPolicyBinding("projects", g.Caravan.Name, member, "roles/owner"); err != nil {
+	if err := g.AddPolicyBinding(ctx, "projects", g.Caravan.Name, member, "roles/owner"); err != nil {
 		t.Errorf("unable to add policy binding: %s\n", err)
 	}
 
-	if err := g.AddPolicyBinding("projects", g.Caravan.Name, member, "roles/owner"); err != nil {
+	if err := g.AddPolicyBinding(ctx, "projects", g.Caravan.Name, member, "roles/owner"); err != nil {
 		t.Errorf("unable to add policy binding: %s\n", err)
 	}
-	if err := g.DeleteServiceAccount(name); err != nil {
+	if err := g.DeleteServiceAccount(ctx, name); err != nil {
 		t.Fatalf("unable to delete service account: %s\n", err)
 	}
 }
 
 func TestGetUser(t *testing.T) {
-	c, err := caravan.NewConfigFromScratch("andrea-test-008", "gcp", "europe-west6")
+	c, err := cli.NewConfigFromScratch("andrea-test-015", "gcp", "europe-west6")
 	if err != nil {
 		t.Fatalf("unable to create config: %s\n", err)
 	}
 
-	g, err := gcp.New(c)
+	ctx := context.Background()
+	g, err := gcp.New(ctx, c)
 	if err != nil {
 		t.Fatalf("unable to create GCP: %s\n", err)
 	}
